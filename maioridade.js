62573668d96d32829e7fa79c14d89a9b68be96b2
DM0R3N0 <dfm84@hotmail.com>
2021-11-04 07:53:30 -0300

    Update

---
+++ maioridade.js
@@ -0,0 +1,5 @@
+//Checando se usuário é maior de idade
+
+let idade = prompt('Qual é a sua idade?');
+
+let maioridade = idade >= 18 ? alert('Maior de idade') : alert('Menor de idade'); //operador ternário
